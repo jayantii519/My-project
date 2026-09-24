8b6337f6ff8a93fb99873ec4d863949506e450f9
jayantii519 <jayantijayashree9793@gmail.com>
2026-09-24 19:39:29 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+  public static void main(String[] args) {
+   String name="jayanti";
+   String gender = "Female";
+   int age=18;
+    System.out.println(age);
+     System.out.println(name);
+      System.out.println(gender);
+  }
+}
