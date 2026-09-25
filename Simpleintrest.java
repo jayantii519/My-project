5a4fba3e3de143e9439fdeb401f9293f90c774af
jayantii519 <jayantijayashree9793@gmail.com>
2026-09-25 23:18:01 +0530

    Describe your change

---
+++ Simpleintrest.java
@@ -0,0 +1,10 @@
+public class Simpleintrest {
+  public static void main(String[] args){
+
+  float p = 6, t = 10, r = 3, SI;
+  SI = (p * t * r)/100; 
+  System.out.println("simple intrest is: ");
+  System.out.println("SI= " +SI);
+
+  } 
+}
