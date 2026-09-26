571b82b20a7c135eb8fef947119efae2355bf100
jayantii519 <jayantijayashree9793@gmail.com>
2026-09-26 23:08:01 +0530

    Describe the change

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+public class Evenodd {
+     public static void main(String[] args){
+      int n=9; 
+    if(n % 2 == 0){
+      System.out.println("Number is even");
+    }
+    else{
+     System.out.println("Number is odd");
+    }
+
+     }
+}
