36566802ed3ad46ea7f91b0471ed9cfd0992764f
jayantii519 <jayantijayashree9793@gmail.com>
2026-09-26 23:00:09 +0530

    Descibe the change

---
+++ Positivenegative.java
@@ -0,0 +1,14 @@
+public class Positivenegative {
+     public static void main(String[] args){
+    int n=9 ;
+
+     if(n>0){
+System.out.println("number is positive");}
+     else if (n<0)
+System.out.println("number is negative");      
+    else {
+      System.out.println("number is zero");
+    }
+
+      }
+}
